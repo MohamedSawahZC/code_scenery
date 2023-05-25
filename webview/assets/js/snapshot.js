fec43d96c52712d2c5244a2353515c2902e144ea
MohamedSawahZC <sawah810@gmail.com>
2023-05-26 01:17:27 +0300

    Apply downloading with multiple formats

--- webview/assets/js/snapshot.js
+++ webview/assets/js/snapshot.js
@@ -1,43 +1,50 @@
 const snapshotContainerNode = document.querySelector('.snapshot-container');
 const snapshotContainerBackgroundNode = document.querySelector('.snapshot-container__background');
 const terminalNode = document.querySelector('.terminal');
+const exportFormatNode = document.getElementById('export-format');
 
 export const takeSnapshot = (filename) => {
-    snapshotContainerNode.style.resize = 'none';
-    terminalNode.style.resize = 'none';
-    const exportSvgNode = document.getElementById('export-svg');
+  snapshotContainerNode.style.resize = 'none';
+  terminalNode.style.resize = 'none';
 
-    const resetStyles = () => {
-        snapshotContainerNode.style.resize = '';
-        terminalNode.style.resize = '';
-    };
-    const options = {
-        width: snapshotContainerBackgroundNode.offsetWidth * 2,
-        height: snapshotContainerBackgroundNode.offsetHeight * 2,
-        style: {
-            transform: 'scale(2)',
-            'transformOrigin': 'center',
-            background: '#e0eafc',
-            background: 'linear-gradient(to left, #e0eafc, #cfdef3);'
-        }
-    };
+  const resetStyles = () => {
+    snapshotContainerNode.style.resize = '';
+    terminalNode.style.resize = '';
+  };
 
-    if (exportSvgNode.checked) {
-        domtoimage
-            .toSvg(snapshotContainerBackgroundNode, options)
-            .then(function (dataUrl) {
-                resetStyles();
-                var link = document.createElement('a');
-                link.download = `codescenery-${filename}.svg`;
-                link.href = dataUrl;
-                link.click();
-            });
-    } else {
-        domtoimage
-            .toBlob(snapshotContainerBackgroundNode, options)
-            .then(function (blob) {
-                resetStyles();
-                window.saveAs(blob, `codescenery-${filename}.png`);
-            });
+  const options = {
+    width: snapshotContainerBackgroundNode.offsetWidth * 2,
+    height: snapshotContainerBackgroundNode.offsetHeight * 2,
+    style: {
+      transform: 'scale(2)',
+      transformOrigin: 'center',
+      background: '#e0eafc',
+      background: 'linear-gradient(to left, #e0eafc, #cfdef3)'
     }
+  };
+
+  const selectedValue = exportFormatNode.value;
+
+  if (selectedValue === 'svg') {
+    domtoimage.toSvg(snapshotContainerBackgroundNode, options)
+      .then(function (dataUrl) {
+        resetStyles();
+        const link = document.createElement('a');
+        link.download = `codescenery-${filename}.svg`;
+        link.href = dataUrl;
+        link.click();
+      });
+  } else if(selectedValue === 'png') {
+    domtoimage.toBlob(snapshotContainerBackgroundNode, options)
+      .then(function (blob) {
+        resetStyles();
+        window.saveAs(blob, `codescenery-${filename}.${selectedValue}`);
+      });
+  }else{
+    domtoimage.toBlob(snapshotContainerBackgroundNode, options)
+      .then(function (blob) {
+        resetStyles();
+        window.saveAs(blob, `codescenery-${filename}.jpg`);
+      });
+  }
 };
